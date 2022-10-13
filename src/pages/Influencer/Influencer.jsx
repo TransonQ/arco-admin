31414860f893d943b6d1eb911d349bc7241a0f44
quanscheng <quanscheng@163.com>
2022-10-13 21:28:55 +0800

    feat: 表格案例

--- src/pages/Influencer/Influencer.jsx
+++ src/pages/Influencer/Influencer.jsx
@@ -1,4 +1,15 @@
-import { Card, PageHeader, Table, Tabs } from '@arco-design/web-react'
+import {
+  Button,
+  Card,
+  Divider,
+  Grid,
+  Input,
+  PageHeader,
+  Select,
+  Space,
+  Table,
+  Tabs,
+} from '@arco-design/web-react'
 import { useState } from 'react'
 
 const columns = [
@@ -70,21 +81,62 @@ export const Influencer = () => {
       <Card bordered={false}>
         <Tabs defaultActiveTab="1" size="large" type="rounded">
           <Tabs.TabPane key="1" title="All">
-            <Table
-              rowKey="id"
-              columns={columns}
-              data={data}
-              rowSelection={{
-                selectedRowKeys,
-                onChange: (selectedRowKeys, selectedRows) => {
-                  // console.log('onChange:', selectedRowKeys, selectedRows)
-                  setSelectedRowKeys(selectedRowKeys)
-                },
-                onSelect: (selected, record, selectedRows) => {
-                  // console.log('onSelect:', selected, record, selectedRows)
-                },
-              }}
-            />
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <Input.Group compact style={{ display: 'flex' }}>
+                <Select
+                  size="large"
+                  defaultValue="Beijing"
+                  showSearch
+                  allowClear
+                  style={{ width: '15%' }}
+                >
+                  <Select.Option value="Beijing">Beijing</Select.Option>
+                  <Select.Option value="Tianjin">Tianjin</Select.Option>
+                  <Select.Option value="Shanghai">Shanghai</Select.Option>
+                </Select>
+                <Input.Search
+                  size="large"
+                  style={{ width: '85%' }}
+                  placeholder="Search"
+                  allowClear
+                />
+                <Button size="large" type="primary">
+                  More filters
+                </Button>
+              </Input.Group>
+
+              <Grid.Row>
+                <Grid.Col span={7}>
+                  Platform
+                  <Select size="small" style={{ width: '100%' }}></Select>
+                </Grid.Col>
+                <Divider type="vertical" style={{ height: '100%' }} />
+                <Grid.Col span={7}>
+                  Language <Select size="small"></Select>
+                </Grid.Col>
+                <Divider type="vertical" style={{ height: '100%' }} />
+                <Grid.Col span={7}>
+                  Tier <Select size="small"></Select>
+                </Grid.Col>
+              </Grid.Row>
+
+              <Divider />
+              <Table
+                rowKey="id"
+                columns={columns}
+                data={data}
+                rowSelection={{
+                  selectedRowKeys,
+                  onChange: (selectedRowKeys, selectedRows) => {
+                    // console.log('onChange:', selectedRowKeys, selectedRows)
+                    setSelectedRowKeys(selectedRowKeys)
+                  },
+                  onSelect: (selected, record, selectedRows) => {
+                    // console.log('onSelect:', selected, record, selectedRows)
+                  },
+                }}
+              />
+            </Space>
           </Tabs.TabPane>
           <Tabs.TabPane key="2" title="Youtube"></Tabs.TabPane>
           <Tabs.TabPane key="3" title="Instagram"></Tabs.TabPane>
